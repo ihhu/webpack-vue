df9c61c9777415451a93100b115f79157ea78bd6
_ihhu <724415664@qq.com>
2019-08-15 14:04:46 +0800

    升级依赖包 升级style-loader配置选项

--- build/dev.js
+++ build/dev.js
@@ -9,82 +9,83 @@ const {
     OUTPUT_PATH_IMAGE
 } = require("./config.js");
 
-const baseConf=require("./base");
-const devConf={
-    mode:"development",
-    output:{
-        path:OUTPUT_PATH,
-        chunkFilename:`${OUTPUT_PATH_JS}[name].js`,
-        filename:`${OUTPUT_PATH_JS}app.[name].js`
-    },
-    optimization:{
-    },
-    module:{
-        rules:[
-            {
-                test: /\.css|scss$/,
-                use:[
-                    {
-                        loader:"style-loader",
-                        options:{
-                            sourceMap:true
+function webpackConfig(env={}){
+    const baseConf=require("./base");
+    const devConf={
+        mode:"development",
+        output:{
+            path:OUTPUT_PATH,
+            chunkFilename:`${OUTPUT_PATH_JS}[name].js`,
+            filename:`${OUTPUT_PATH_JS}app.[name].js`
+        },
+        optimization:{
+        },
+        module:{
+            rules:[
+                {
+                    test: /\.css|scss$/,
+                    use:[
+                        {
+                            loader:"style-loader",
+                            options:{}
+                        },
+                        {
+                            loader:"css-loader",
+                            options:{
+                                sourceMap:true
+                            }
+                        },
+                        {
+                            loader:"postcss-loader",
+                            options:{
+                                sourceMap:true
+                            }
+                        },
+                        {
+                            loader:"sass-loader",
+                            options:{
+                                sourceMap:true
+                            }
                         }
-                    },
-                    {
-                        loader:"css-loader",
-                        options:{
-                            sourceMap:true
+                    ]
+                }, 
+                {
+                    test: /\.(eot|ttf|woff|woff2)\w*/,
+                    use: [{
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: OUTPUT_PATH_FONT,
                         }
-                    },
-                    {
-                        loader:"postcss-loader",
-                        options:{
-                            sourceMap:true
+                    }]
+                }, 
+                {
+                    test: /\.(jpg|jpeg|png|gif|svg|ico)$/i,
+                    use: [{
+                        loader: "url-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            limit: 8192,
+                            outputPath: OUTPUT_PATH_IMAGE,
                         }
-                    },
-                    {
-                        loader:"sass-loader",
-                        options:{
-                            sourceMap:true
-                        }
-                    }
-                ]
-            }, 
-            {
-                test: /\.(eot|ttf|woff|woff2)\w*/,
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                        outputPath: OUTPUT_PATH_FONT,
-                    }
-                }]
-            }, 
-            {
-                test: /\.(jpg|jpeg|png|gif|svg|ico)$/i,
-                use: [{
-                    loader: "url-loader",
-                    options: {
-                        name: "[name].[ext]",
-                        limit: 8192,
-                        outputPath: OUTPUT_PATH_IMAGE,
-                    }
-                }]
-            }
+                    }]
+                }
+            ]
+        },
+        devtool:"cheap-module-eval-source-map",
+        plugins:[
+        ],
+        devServer:{
+            compress:true,
+            //open:true,
+        },
+        plugins:[
+            new webpack.DefinePlugin({
+                IS_DEV: JSON.stringify(true)
+            })
         ]
-    },
-    devtool:"cheap-module-eval-source-map",
-    plugins:[
-    ],
-    devServer:{
-        compress:true,
-        //open:true,
-    },
-    plugins:[
-        new webpack.DefinePlugin({
-            IS_DEV: JSON.stringify(true)
-        })
-    ]
-};
+    };
 
-module.exports=webpackMerge(baseConf,devConf)
+    return webpackMerge(baseConf,devConf);
+}
+module.exports=webpackConfig
