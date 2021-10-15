eacaf3c434519737aa9c3562ad71c1dc6b23cf47
_ihhu <v.ihhu@qq.com>
2021-10-15 13:34:38 +0800

    依赖包更新

--- src/pages/index/main.tsx
+++ src/pages/index/main.tsx
@@ -3,19 +3,17 @@ import '@assets/scss/style.scss';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-
-
 console.log('IS_DEV:::', IS_DEV);
 console.log('app', App);
 
 const app = createApp({
-  data(){
+  data() {
     return {
-      text:424323
+      text: 424323,
     };
   },
   // router,
-  mounted(){
+  mounted() {
     console.log(IS_DEV);
     const oApp = document.querySelector('#app') as Element;
     const oImg = require('@assets/images/13.jpg');
@@ -26,52 +24,49 @@ const app = createApp({
     oImgEle.style.backgroundImage = `url(${oImg})`;
     oApp.appendChild(oImgEle);
   },
-  render(){
-    return <App/>;
-  }
-    
+  render() {
+    return <App />;
+  },
 });
 
 app.use(router);
 app.mount('#app');
 
-const a = new Promise(function(resolve) {
+const a = new Promise(function (resolve) {
   resolve(void 0);
 });
 const b = new Map();
 b.set(a, 'Promise');
 
-
-async function cc(){
-  await a.then(()=>{
+async function cc() {
+  await a.then(() => {
     console.log('Promise aa');
   });
   console.log('await ccccc');
 }
 cc();
 
-class D{
-    a:number;
-    b='bb';
+class D {
+  a: number;
+  b = 'bb';
 
-    constructor(){
-      this.a = 0;
-    }
-    setA(){
-      this.a = 10;
-    }
+  constructor() {
+    this.a = 0;
+  }
+  setA() {
+    this.a = 10;
+  }
 }
 const d = new D();
 console.log('class dd', d, d.b, d.a);
 d.setA();
 console.log('class d.a', d.a);
 console.log('map', b.get(a));
-console.log([1, 4, -5, 10].find((n) => n < 0));
-
+console.log([1, 4, -5, 10].find(n => n < 0));
 
 // 启用热更新
 /* eslint-disable */
-if((module as any).hot){
+if ((module as any).hot) {
   (module as any).hot.accept();
 }
 /* eslint-disable */
